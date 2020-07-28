87f561ea4274df4f919281d3cb2b62ac398dd382
huadie <1275557177@qq.com>
2020-07-28 11:09:03 +0800

    项目配置

---
+++ Desktop/ebook/vue-ebook/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  .icon-bookmark {
+    font-size: 30px;
+  }
+</style>
